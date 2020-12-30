388b06ef58ac3983439c3e023058447902777478
Michał Biarda <m.biarda@gmail.com>
2020-12-30 12:33:46 +0100

    Get return labels method

---
+++ src/Method/Shipment/GetReturnLabels.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Copyright © Michał Biarda. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+namespace MB\ShipXSDK\Method\Shipment;
+
+use MB\ShipXSDK\Method\MethodInterface;
+use MB\ShipXSDK\Method\WithAuthorizationInterface;
+use MB\ShipXSDK\Method\WithBinaryResponseInterface;
+use MB\ShipXSDK\Method\WithQueryParamsInterface;
+use MB\ShipXSDK\Request\Request;
+
+class GetReturnLabels implements
+    MethodInterface,
+    WithBinaryResponseInterface,
+    WithAuthorizationInterface,
+    WithQueryParamsInterface
+{
+    public function getRequestMethod(): string
+    {
+        return Request::METHOD_GET;
+    }
+
+    public function getUriTemplate(): string
+    {
+        return '/v1/organizations/:organization_id/shipments/return_labels';
+    }
+
+    public function getRequiredQueryParams(): array
+    {
+        return ['shipment_ids'];
+    }
+
+    public function getOptionalQueryParams(): array
+    {
+        return ['format'];
+    }
+}
